3a651c0460e5bbe7b893152838e7fcece5b88fb2
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-21 09:53:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make = make;
+	this.model = model;
+}
+Car.prototype.getMakeModel = function () {
+	return `${this.make} ${this.model}`;
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	this.make = make;
+	this.model = model;
+	this.topSpeed = topSpeed;
+}
+SportsCar.prototype.getTopSpeed = function () {
+	return `${this.topSpeed}`;
+}
 
 // Do not change the code below
 window.Car = Car;
